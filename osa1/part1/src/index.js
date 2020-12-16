7399af5ff2bbe08f9ffc2aef2dd3d80412634512
zeeket <3qvm@tutanota.com>
2020-12-16 16:41:08 +0200

    props

--- osa1/part1/src/index.js
+++ osa1/part1/src/index.js
@@ -1,19 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
+const Hello = (props) => {
+    return (
+        <div>
+        <p>
+        Hello {props.name}, you are {props.age} years old
+        </p>
+        </div>
+    )
+}
 
-  return (
-    <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
+const App = () => {
+    const now = new Date()
+    const a = 10
+    const b = 20
+    const nimi = 'Pekka'
+    const ika = 10
+
+    return (
+        <div>
+        <p>Hello world, it is {now.toString()}</p>
+        <p>
         {a} plus {b} is {a + b}
-      </p>
-    </div>
-  )
+        </p>
+        <h1>Greetings</h1>
+        <Hello name="Maya" age={26 + 10} />
+        <Hello name={nimi} age={ika} />
+        </div>
+    )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
