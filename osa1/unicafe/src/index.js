857adcef96adcc420b4fe4c387927b54a22fce49
zeeket <3qvm@tutanota.com>
2020-12-27 01:43:43 +0200

    1.7 tehty

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -11,11 +11,21 @@ const Counter = (props) => (
     <p>{props.text} {props.value}</p>
 )
 
+const getDivisor = all => {
+    if(Math.abs(all)>0){
+        console.log("math abs is ",Math.abs(all))
+    }
+  return Math.abs(all)>0 ? all:1;
+};
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good+neutral+bad
+  const average = (good-bad)/(all?all:1)
+  const positive = (good/(all?all:1))*100+"%"
 
   return (
     <div>
@@ -26,6 +36,9 @@ const App = () => {
       <Counter text="good" value={good}/>
       <Counter text="neutral" value={neutral}/>
       <Counter text="bad" value={bad}/>
+      <Counter text="all" value={all}/>
+      <Counter text="average" value={average}/>
+      <Counter text="positive" value={positive}/>
     </div>
 
   )
