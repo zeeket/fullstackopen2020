4944cfd57c2672a2d9639c9a0b0a03a3e1627aa9
zeeket <3qvm@tutanota.com>
2020-12-27 02:07:32 +0200

    1.8 tehty

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 
 const Button = (props) => (
     <button onClick={props.hc}>
-        {props.text}
+    {props.text}
     </button>
 )
 
@@ -11,39 +11,40 @@ const Counter = (props) => (
     <p>{props.text} {props.value}</p>
 )
 
-const getDivisor = all => {
-    if(Math.abs(all)>0){
-        console.log("math abs is ",Math.abs(all))
-    }
-  return Math.abs(all)>0 ? all:1;
-};
+const Statistics = (props) => {
+    let all = props.good+props.neutral+props.bad;
+    let average = (props.good-props.bad)/(all?all:1);
+    let positive = (props.good/(all?all:1))*100+"%";
+    return (
+        <>
+        <Counter text="good" value={props.good}/>
+        <Counter text="neutral" value={props.neutral}/>
+        <Counter text="bad" value={props.bad}/>
+        <Counter text="all" value={all}/>
+        <Counter text="average" value={average}/>
+        <Counter text="positive" value={positive}/>
+        </>
+    )
+}
 
 const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const all = good+neutral+bad
-  const average = (good-bad)/(all?all:1)
-  const positive = (good/(all?all:1))*100+"%"
+    // tallenna napit omaan tilaansa
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
 
-  return (
-    <div>
-      <h1>give feedback</h1>
-      <Button hc={()=>setGood(good+1)} text="good"/ >
-      <Button hc={()=>setNeutral(neutral+1)} text="neutral"/ >
-      <Button hc={()=>setBad(bad+1)} text="bad"/ >
-      <Counter text="good" value={good}/>
-      <Counter text="neutral" value={neutral}/>
-      <Counter text="bad" value={bad}/>
-      <Counter text="all" value={all}/>
-      <Counter text="average" value={average}/>
-      <Counter text="positive" value={positive}/>
-    </div>
+    return (
+        <div>
+        <h1>give feedback</h1>
+        <Button hc={()=>setGood(good+1)} text="good"/ >
+        <Button hc={()=>setNeutral(neutral+1)} text="neutral"/ >
+        <Button hc={()=>setBad(bad+1)} text="bad"/ >
+        <Statistics good={good} neutral={neutral} bad={bad}/>
+        </div>
 
-  )
+    )
 }
 
 ReactDOM.render(<App />, 
-  document.getElementById('root')
+    document.getElementById('root')
 )
