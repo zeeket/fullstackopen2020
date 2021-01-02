8ea90ddee05c583cf445822c1949e0d450f6ca7b
e <baudel@rala.local>
2021-01-02 22:46:52 +0200

    organize course code away from index.js

---
+++ osa2/kurssitiedot/src/Course.js
@@ -0,0 +1,48 @@
+
+import React from 'react'
+
+const Header = (props) => {
+    return (
+        <>
+        <h1>{props.course}!</h1>
+        </>
+    )
+}
+
+const Content = (props) => {
+    return (
+        <div>
+        {props.parts.map((part) => 
+            <Part name = {part.name} exercises = {part.exercises} />
+        )}
+            </div>
+        )
+        }
+
+        const Part = (props) => {
+            return (
+                <>
+                <p>{props.name} {props.exercises}</p>
+                </>
+            )
+        }
+
+        const Total = (props) => {
+            const sumTotal = props.parts.reduce((totalSum, part) => totalSum + part.exercises, 0);
+            return (
+                <>
+                <p>Number of exercises {sumTotal}</p>
+                </>
+            )
+        }
+
+const Course = ({ course }) => {
+  return (
+                <div>
+                <Header course={course.name} />
+                <Content parts={course.parts} />
+                <Total parts={course.parts}/>
+                </div>
+  )
+}
+export default Course
--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,41 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Header = (props) => {
-    return (
-        <>
-        <h1>{props.course}!</h1>
-        </>
-    )
-}
-
-const Content = (props) => {
-    return (
-        <div>
-        {props.parts.map((part) => 
-            <Part name = {part.name} exercises = {part.exercises} />
-        )}
-            </div>
-        )
-        }
-
-        const Part = (props) => {
-            return (
-                <>
-                <p>{props.name} {props.exercises}</p>
-                </>
-            )
-        }
-
-        const Total = (props) => {
-            const sumTotal = props.parts.reduce((totalSum, part) => totalSum + part.exercises, 0);
-            return (
-                <>
-                <p>Number of exercises {sumTotal}</p>
-                </>
-            )
-        }
-
+import Course from './Course'
+    
         const App = () => {
             const course = {
                 name: 'Half Stack application development',
@@ -59,11 +25,7 @@ const Content = (props) => {
                 ]
             }
             return (
-                <div>
-                <Header course={course.name} />
-                <Content parts={course.parts} />
-                <Total parts={course.parts}/>
-                </div>
+                <Course course={course} />
             )
         }
 
