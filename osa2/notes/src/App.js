6d25cbf0acc55c7c9001bd0f061709be724e450d
zeeket <emil.heliste@helsinki.fi>
2021-01-03 17:35:39 +0200

    notes kokeiluja

--- osa2/notes/src/App.js
+++ osa2/notes/src/App.js
@@ -1,15 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props)=> {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  ) 
+  const [showAll, setShowAll] = useState(true)
+
+const addNote = (event) => {
+  event.preventDefault()
+  const noteObject = {
+    content: newNote,
+    date: new Date().toISOString(),
+    important: Math.random() > 0.5,
+    id: notes.length + 1,
+  }
+    console.log('setting note',noteObject)
+  setNotes(notes.concat(noteObject))
+  setNewNote('')
+}
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>  
       <ul>
-        {notes.map((note, i) => 
-          <Note key={note.id} note={note} />
+        {notesToShow.map(note => 
+        <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input 
+        value={newNote}
+        onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>   
     </div>
   )
 }
