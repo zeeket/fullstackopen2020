f58626b21a1f7693b0d588b8ce70117c044cda31
zeeket <emil.heliste@helsinki.fi>
2021-01-10 00:04:51 +0200

    2.19 tehty

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,12 +3,14 @@ import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import personService from './services/persons'
+import Notification from './components/Notification'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [nameFilter, setNameFilter] = useState('')
+  const [notifMessage, setNotifMessage] = useState(null)
 
   const hook = () => {
     personService
@@ -26,16 +28,38 @@ const App = () => {
       name: newName,
       number: newNumber
     }
-
-    if(!persons.some(p => p.name === newName)) {
+    const sameName = persons.find(p => p.name === newName)
+    if(!sameName) {
       personService
         .create(nameObject)
         .then(returnedPerson => {
           setPersons(persons.concat(returnedPerson))
           setNewName('')
           setNewNumber('')
+          setNotifMessage(
+            `${returnedPerson.name} was added`
+          )
+          setTimeout(() => {
+            setNotifMessage(null)
+          }, 5000)
         })
-    } else { 
+
+    } else if(sameName.number !== newNumber){
+      personService
+        .update(sameName.id,{...sameName, number:newNumber})
+        .then(returnedPerson => {
+          setPersons(persons.map(p => p.id === returnedPerson.id ? returnedPerson : p ))
+          setNewName('')
+          setNewNumber('')
+          setNotifMessage(
+            `${returnedPerson.name}'s number was changed`
+          )
+          setTimeout(() => {
+            setNotifMessage(null)
+          }, 5000)
+        })
+    } else {
+
       alert(`${newName} is already added to phonebook`)
     }
   }
@@ -53,8 +77,16 @@ const App = () => {
   }
 
   const handleDeletePerson = (id) => {
-    if(window.confirm(`Are you sure you want to delete ${persons.find(person => person.id == id).name} ?`)){
+    const tbd = persons.find(person => person.id === id).name
+    if(window.confirm(`Are you sure you want to delete ${tbd} ?`)){
       personService.deletePerson(id)
+
+      setNotifMessage(
+        `${tbd} was deleted`
+      )
+      setTimeout(() => {
+        setNotifMessage(null)
+      }, 5000)
       setPersons(persons.filter(person => person.id !== id))
     }
   }
@@ -67,6 +99,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notifMessage} />
       <Filter nameFilter={nameFilter} handleNameFilterChange={handleNameFilterChange}/>
       <h3>Add a new</h3>
       <PersonForm addName={addName} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}/>
