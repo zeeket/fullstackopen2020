94393e88f24f07c3b0b69a77776636c1bf9981d3
zeeket <emil.heliste@helsinki.fi>
2021-03-01 16:44:09 +0200

    osa 4 tehty

--- osa4/app.js
+++ osa4/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
+require('express-async-errors')
 const app = express()
+const middleware = require('./utils/middleware')
 
 app.use(cors())
 app.use(express.json())
@@ -10,5 +12,11 @@ app.use('/api/blogs', blogsRouter)
 
 const usersRouter = require('./controllers/users')
 app.use('/api/users', usersRouter)
+
+const loginRouter = require('./controllers/login')
+app.use('/api/login', loginRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
