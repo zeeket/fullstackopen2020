31d69b437ed0634657dd2a4dd42556e47dec58a6
zeeket <emil.heliste@helsinki.fi>
2021-03-01 18:13:55 +0200

    teht 5.3 tehty

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -9,6 +9,10 @@ const App = () => {
   const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null)
 
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
@@ -21,6 +25,7 @@ const App = () => {
       console.log("logged in via localStorage")
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
+      blogService.setToken(user.token)
     }
   }, [])
 
@@ -35,6 +40,9 @@ const App = () => {
       window.localStorage.setItem(
         'loggedBloglistappUser', JSON.stringify(user)
       ) 
+      console.log("set localstorage item to ",JSON.stringify(user))
+
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -50,6 +58,21 @@ const App = () => {
     setUser(null)
     setUsername('')
     setPassword('')
+  }
+
+  const handleSubmitBlog = async (event) => {
+    event.preventDefault()
+
+    try {
+      const res = await blogService.create({
+        title, author, url, likes:0
+      })
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+    } catch (exception) {
+      console.log("error submitting blog")
+    }
   }
 
   const loginForm = () => (
@@ -76,6 +99,39 @@ const App = () => {
     </form>      
   )
 
+  const blogForm = () => (
+    <form onSubmit={handleSubmitBlog}>
+      <div>
+        title:
+        <input
+          type="text"
+          value={title}
+          name="title"
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+      <div>
+        author: 
+        <input
+          type="text"
+          value={author}
+          name="author"
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+      </div>
+      <div>
+        url: 
+        <input
+          type="text"
+          value={url}
+          name="url"
+          onChange={({ target }) => setUrl(target.value)}
+        />
+      </div>
+      <button type="submit">create</button>
+    </form>      
+  )
+
   return (
     <div>
       <h2>blogs</h2>
@@ -91,6 +147,7 @@ const App = () => {
         </form>      
       </div>
       }
+      {user !==null && blogForm()}
       {user !== null && blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
--- osa5/bloglist-frontend/src/services/blogs.js
+++ osa5/bloglist-frontend/src/services/blogs.js
@@ -3,9 +3,22 @@ const baseUrl = '/api/blogs'
 
 let token = null
 
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-export default { getAll }
+const create = async newObject => {
+  const config = {
+    headers: { Authorization: token },
+  }
+console.log("config is ",config)
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
+}
+
+export default { getAll, create, setToken }
