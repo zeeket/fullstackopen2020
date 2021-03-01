ce5217fa67ea981b74d403842784e3e12cc39388
zeeket <emil.heliste@helsinki.fi>
2021-03-01 20:32:56 +0200

    5.7 tehty! pakollinen tahtitehtava yksi?

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,19 @@
-import React from 'react'
-const Blog = ({ blog }) => (
+import React, { useRef } from 'react'
+import Togglable from './Togglable'
+
+const Blog = ({ blog }) => {
+  const blogRef = useRef()
+
+ return (
   <div>
     {blog.title} {blog.author}
+    <Togglable buttonLabel="view" ref={blogRef}>
+      <p>{blog.url}</p>
+      <p>likes {blog.likes}</p>
+      <p>{blog.user.name}</p>
+    </Togglable>
   </div>
 )
+}
 
 export default Blog
