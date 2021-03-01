a01bad5c6a470bc50b258a3b4c1e7c4068caf0c6
zeeket <emil.heliste@helsinki.fi>
2021-03-01 17:31:10 +0200

    teht 5.1 tehty

---
+++ osa5/bloglist-frontend/src/App.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log("wrong credentials")
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>      
+  )
+
+  return (
+    <div>
+      <h2>blogs</h2>
+
+      {user === null && loginForm()}
+
+      {user !== null && <div>
+        <p>{user.name} logged in</p>
+      </div>
+      }
+          {user !== null && blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+          )}
+          </div>
+      )
+      }
+
+      export default App
