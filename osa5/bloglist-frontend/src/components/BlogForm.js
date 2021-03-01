a7e80d1d08fc89625fafbd08edcc405d0c97c727
zeeket <emil.heliste@helsinki.fi>
2021-03-01 18:44:14 +0200

    5.5 & 5.6 tehty

---
+++ osa5/bloglist-frontend/src/components/BlogForm.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import blogService from '../services/blogs'
+
+  const BlogForm = ({createBlog}) => {
+
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  const handleSubmitBlog = async (event) => {
+    event.preventDefault()
+    try {
+      const res = await blogService.create({
+        title, author, url, likes:0
+      })
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+    } catch (exception) {
+      console.log("error submitting blog")
+    }
+  }
+
+    return (
+    <form onSubmit={handleSubmitBlog}>
+      <div>
+        title:
+        <input
+          type="text"
+          value={title}
+          name="title"
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+      <div>
+        author: 
+        <input
+          type="text"
+          value={author}
+          name="author"
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+      </div>
+      <div>
+        url: 
+        <input
+          type="text"
+          value={url}
+          name="url"
+          onChange={({ target }) => setUrl(target.value)}
+        />
+      </div>
+      <button type="submit">create</button>
+    </form>      
+  )
+}
+
+export default BlogForm
