e58bda1d281485bdd69df97a1051d21e39d9f978
zeeket <emil.heliste@helsinki.fi>
2021-03-01 15:09:19 +0200

    teht 4.10 tehty

--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -28,6 +28,20 @@ test('all blogs are returned', async () => {
   expect(response.body).toHaveLength(initialBlogs.length)
 })
 
+test('a valid blog can be added ', async () => {
+  const newBlog = { title: "Type wars", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html", likes: 2 }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(initialBlogs.length + 1)
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
